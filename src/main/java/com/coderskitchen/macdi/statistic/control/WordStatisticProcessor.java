ba9c4ca9081dfaebb818e760f0fcb01d8606c0d2
Peter Daum <daum@tyntec.com>
2013-03-30 00:38:19 +0100

    Fixed bug, that special characters were also recognized

--- src/main/java/com/coderskitchen/macdi/statistic/control/WordStatisticProcessor.java
+++ src/main/java/com/coderskitchen/macdi/statistic/control/WordStatisticProcessor.java
@@ -3,29 +3,31 @@ package com.coderskitchen.macdi.statistic.control;
 import java.util.HashMap;
 
 public class WordStatisticProcessor {
-    public HashMap<String, Integer> deriveStatisticFrom(String document) {
-        HashMap<String, Integer> statistic = new HashMap<String, Integer>();
-        if (isDocumentNotEmpty(document)) {
-            splitDocumentAndAddWords(document, statistic);
-        }
-        return statistic;
-    }
+	public HashMap<String, Integer> deriveStatisticFrom(String document) {
+		HashMap<String, Integer> statistic = new HashMap<String, Integer>();
+		if (isDocumentNotEmpty(document)) {
+			splitDocumentAndAddWords(document, statistic);
+		}
+		return statistic;
+	}
 
-    private void splitDocumentAndAddWords(String document, HashMap<String, Integer> statistic) {
-        String[] words = document.split(" ");
-        for (String word : words) {
-            incrementWordCount(statistic, word.toLowerCase());
-        }
-    }
+	private void splitDocumentAndAddWords(String document, HashMap<String, Integer> statistic) {
+		document = document.replaceAll("[^A-Za-z0-9]", " ");
+		String[] words = document.split(" ");
+		for (String word : words) {
+			if (!word.isEmpty())
+				incrementWordCount(statistic, word.toLowerCase());
+		}
+	}
 
-    private void incrementWordCount(HashMap<String, Integer> statistic, String word) {
-        if(!statistic.containsKey(word))
-            statistic.put(word, 0);
-        int incremented = statistic.get(word) + 1;
-        statistic.put(word, incremented);
-    }
+	private void incrementWordCount(HashMap<String, Integer> statistic, String word) {
+		if (!statistic.containsKey(word))
+			statistic.put(word, 0);
+		int incremented = statistic.get(word) + 1;
+		statistic.put(word, incremented);
+	}
 
-    private boolean isDocumentNotEmpty(String document) {
-        return document != null && !document.isEmpty();
-    }
+	private boolean isDocumentNotEmpty(String document) {
+		return document != null && !document.isEmpty();
+	}
 }
--- src/test/java/com/coderskitchen/macdi/statistic/control/WordStatisticProcessorTest.java
+++ src/test/java/com/coderskitchen/macdi/statistic/control/WordStatisticProcessorTest.java
@@ -1,64 +1,69 @@
 package com.coderskitchen.macdi.statistic.control;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 import java.util.HashMap;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class WordStatisticProcessorTest {
 
-    WordStatisticProcessor cut = new WordStatisticProcessor();
+	WordStatisticProcessor cut = new WordStatisticProcessor();
 
-    @Test
-    public void deriveStatisticsForEmptyDocument() {
-        deriveStatisticsAndAssertSize("", 0);
-    }
+	@Test
+	public void deriveStatisticsForEmptyDocument() {
+		deriveStatisticsAndAssertSize("", 0);
+	}
 
-    @Test
-    public void deriveStatisticsForNullDocument() {
-        deriveStatisticsAndAssertSize(null, 0);
-    }
+	@Test
+	public void deriveStatisticsForNullDocument() {
+		deriveStatisticsAndAssertSize(null, 0);
+	}
 
-    @Test
-    public void deriveStatisticsForOneLetter() {
-        HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A", 1);
-        assertThat(result.get("a"), is(1));
-    }
+	@Test
+	public void deriveStatisticsForOneLetter() {
+		HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A", 1);
+		assertThat(result.get("a"), is(1));
+	}
 
-    @Test
-    public void deriveStatisticsForTwoSingleLetters() {
-        HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A B", 2);
-        assertThatCountIs(result, 1);
-    }
+	@Test
+	public void deriveStatisticsForTwoSingleLetters() {
+		HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A B", 2);
+		assertThatCountIs(result, 1);
+	}
 
-    @Test
-    public void deriveStatisticsForTwoMultiOccurrenceLetters() {
-        HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A A B B C C", 3);
-        assertThatCountIs(result, 2);
-    }
+	@Test
+	public void deriveStatisticsForTwoMultiOccurrenceLetters() {
+		HashMap<String, Integer> result = deriveStatisticsAndAssertSize("A A B B C C", 3);
+		assertThatCountIs(result, 2);
+	}
 
-    @Test
-    public void deriveStatisticsForMixedDocument() {
-        HashMap<String, Integer> result = deriveStatisticsAndAssertSize("The lazy dog hunts the quick brown fox.", 7);
-        for(String key : result.keySet()) {
-            int count = 1;
-            if(key.equalsIgnoreCase("the"))
-                count = 2;
-            assertThat(key, result.get(key), is(count));
-        }
-    }
+	@Test
+	public void deriveStatisticsForDFocumentsWithSpecialChars() {
+		HashMap<String, Integer> result = deriveStatisticsAndAssertSize("{A/ A B B}} C C}", 3);
+		assertThatCountIs(result, 2);
+	}
 
-    private void assertThatCountIs(HashMap<String, Integer> result, int value) {
-        for (String key : result.keySet()) {
-            assertThat(key, result.get(key), is(value));
-        }
-    }
+	@Test
+	public void deriveStatisticsForMixedDocument() {
+		HashMap<String, Integer> result = deriveStatisticsAndAssertSize("The lazy dog hunts the quick brown fox.", 7);
+		for (String key : result.keySet()) {
+			int count = 1;
+			if (key.equalsIgnoreCase("the"))
+				count = 2;
+			assertThat(key, result.get(key), is(count));
+		}
+	}
 
-    private HashMap<String, Integer> deriveStatisticsAndAssertSize(String document, int expectedSize) {
-        HashMap<String, Integer> result = cut.deriveStatisticFrom(document);
-        assertThat(result.size(), is(expectedSize));
-        return result;
-    }
+	private void assertThatCountIs(HashMap<String, Integer> result, int value) {
+		for (String key : result.keySet()) {
+			assertThat(key, result.get(key), is(value));
+		}
+	}
+
+	private HashMap<String, Integer> deriveStatisticsAndAssertSize(String document, int expectedSize) {
+		HashMap<String, Integer> result = cut.deriveStatisticFrom(document);
+		assertThat(result.size(), is(expectedSize));
+		return result;
+	}
 }
